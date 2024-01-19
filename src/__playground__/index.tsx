ffc4077dbd2ae604445aeba869c892b7d65a8c13
dong.huo <huodon@gmail.com>
2024-01-19 16:15:29 +0800

    update playground

--- src/__playground__/index.tsx
+++ src/__playground__/index.tsx
@@ -2,18 +2,8 @@ import { Portal, render } from 'vitro'
 import './index.css'
 
 import { Route, Router } from '@vitro/router'
-import {
-  StackStyles,
-  flex,
-  stack
-} from 'styled-system/patterns'
-import {
-  ButtonVariantProps,
-  IconButtonVariantProps,
-  button,
-  dialog,
-  iconButton,
-} from 'styled-system/recipes'
+import { container, flex, stack } from 'styled-system/patterns'
+import { button, dialog, iconButton } from 'styled-system/recipes'
 
 import { X } from '@vitro/lucide'
 import { css } from 'styled-system/css'
@@ -36,27 +26,7 @@ const Layout = () => {
 
 const dialogStyles = dialog()
 
-const Stack = ({
-  css: stackStyle = {},
-  ...props
-}: JSX.IntrinsicElements['div'] & { css?: StackStyles }) => {
-  return <div {...props} class={[stack(stackStyle), props.class]} />
-}
-
-const Button = ({
-  css: buttonStyles = {},
-  ...props
-}: JSX.IntrinsicElements['button'] & { css?: ButtonVariantProps }) => {
-  return <button {...props} class={[button(buttonStyles), props.class]} />
-}
-
-const IconButton = ({
-  css: iconButtonStyles = {},
-  ...props
-}: JSX.IntrinsicElements['button'] & { css?: IconButtonVariantProps }) => (
-  <button {...props} class={[iconButton(iconButtonStyles), props.class]} />
-)
-
+container({})
 const App = () => {
   return (
     <Router
@@ -64,66 +34,70 @@ const App = () => {
         {
           path: '/',
           to: () => (
-            <div>
-              <h2>Home</h2>
-
-              <Button css={{ variant: 'solid' }}>Docs</Button>
-
-              <Dialog.Dialog>
-                <Dialog.DialogTrigger class={dialogStyles.trigger}>
-                  Open Dialog
-                </Dialog.DialogTrigger>
-                <Portal>
-                  <Dialog.DialogBackdrop class={dialogStyles.backdrop} />
-                  <Dialog.DialogPositioner class={dialogStyles.positioner}>
-                    <Dialog.DialogContent class={dialogStyles.content}>
-                      <Stack css={{ gap: '8', p: '6' }}>
-                        <Stack css={{ gap: '1' }}>
-                          <Dialog.DialogTitle class={dialogStyles.title}>
-                            Dialog Title
-                          </Dialog.DialogTitle>
-                          <Dialog.DialogDescription
-                            class={dialogStyles.description}
+            <div class={[container(), css({ w: 'full', p: '8' })]}>
+              <div class={[container(), flex({ justifyContent: 'center' })]}>
+                <Dialog.Dialog>
+                  <Dialog.DialogTrigger asChild>
+                    <button class={[dialogStyles.trigger, button()]}>
+                      Open Dialog
+                    </button>
+                  </Dialog.DialogTrigger>
+                  <Portal>
+                    <Dialog.DialogBackdrop class={dialogStyles.backdrop} />
+                    <Dialog.DialogPositioner class={dialogStyles.positioner}>
+                      <Dialog.DialogContent class={dialogStyles.content}>
+                        <div class={stack({ gap: '8', p: '6' })}>
+                          <div class={stack({ gap: '1' })}>
+                            <Dialog.DialogTitle class={dialogStyles.title}>
+                              Dialog Title
+                            </Dialog.DialogTitle>
+                            <Dialog.DialogDescription
+                              class={dialogStyles.description}
+                            >
+                              Dialog Description
+                            </Dialog.DialogDescription>
+                          </div>
+                          <div
+                            class={stack({
+                              gap: '3',
+                              direction: 'row',
+                              w: 'full',
+                            })}
                           >
-                            Dialog Description
-                          </Dialog.DialogDescription>
-                        </Stack>
-                        <Stack
-                          css={{
-                            gap: '3',
-                            direction: 'row',
-                            w: 'full',
-                          }}
-                        >
-                          <Dialog.DialogCloseTrigger asChild>
-                            <Button
-                              css={{ variant: 'outline' }}
+                            <Dialog.DialogCloseTrigger asChild>
+                              <button
+                                class={[
+                                  dialogStyles.closeTrigger,
+                                  button({ variant: 'outline' }),
+                                  css({ w: 'full' }),
+                                ]}
+                              >
+                                Cancel
+                              </button>
+                            </Dialog.DialogCloseTrigger>
+                            <button class={[button(), css({ w: 'full' })]}>
+                              Confirm
+                            </button>
+                          </div>
+                          <Dialog.DialogCloseTrigger
+                            asChild
+                            class={[dialogStyles.closeTrigger]}
+                          >
+                            <button
                               class={[
-                                dialogStyles.closeTrigger,
-                                css({ w: 'full' }),
+                                css({ pos: 'absolute', top: '2', right: '2' }),
+                                iconButton({ variant: 'ghost', size: 'sm' }),
                               ]}
                             >
-                              Cancel
-                            </Button>
+                              <X />
+                            </button>
                           </Dialog.DialogCloseTrigger>
-                          <Button class={css({ w: 'full' })}>Confirm</Button>
-                        </Stack>
-                        <Dialog.DialogCloseTrigger
-                          asChild
-                          class={[
-                            dialogStyles.closeTrigger,
-                            css({ pos: 'absolute', top: '2', right: '2' }),
-                          ]}
-                        >
-                          <IconButton css={{ variant: 'ghost', size: 'sm' }}>
-                            <X />
-                          </IconButton>
-                        </Dialog.DialogCloseTrigger>
-                      </Stack>
-                    </Dialog.DialogContent>
-                  </Dialog.DialogPositioner>
-                </Portal>
-              </Dialog.Dialog>
+                        </div>
+                      </Dialog.DialogContent>
+                    </Dialog.DialogPositioner>
+                  </Portal>
+                </Dialog.Dialog>
+              </div>
             </div>
           ),
         },
